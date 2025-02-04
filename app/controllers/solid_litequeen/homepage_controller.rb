91be36324bdca84b79950b590666697ed7d39e46
Vik Borges <v1k@protonmail.com>
2025-02-04 20:27:36 +0700

    🚀

--- app/controllers/solid_litequeen/homepage_controller.rb
+++ app/controllers/solid_litequeen/homepage_controller.rb
@@ -1,6 +1,9 @@
 module SolidLitequeen
   class HomepageController < ApplicationController
     def index
+      @databases = ActiveRecord::Base.configurations.configurations.select do |config|
+        config.adapter == "sqlite3"
+      end
     end
   end
 end
--- solid_litequeen.gemspec
+++ solid_litequeen.gemspec
@@ -23,4 +23,6 @@ Gem::Specification.new do |spec|
   end
 
   spec.add_dependency "rails", ">= 8.0.1"
+
+  spec.add_development_dependency "web-console"
 end
