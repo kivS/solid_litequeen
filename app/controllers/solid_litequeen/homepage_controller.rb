ebe16b08d5bbe0ba875cb5d98aa9e8a70d1233d6
Vik Borges <v1k@protonmail.com>
2025-02-04 20:33:48 +0700

    🚀

--- app/controllers/solid_litequeen/homepage_controller.rb
+++ app/controllers/solid_litequeen/homepage_controller.rb
@@ -2,7 +2,7 @@ module SolidLitequeen
   class HomepageController < ApplicationController
     def index
       @databases = ActiveRecord::Base.configurations.configurations.select do |config|
-        config.adapter == "sqlite3"
+        config.adapter == "sqlite3" && config.env_name == Rails.env && config.database.present?
       end
     end
   end
