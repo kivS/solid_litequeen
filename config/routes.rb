4d7aee7412f5ccffdd9eac8134ef4bca5ce101b0
Vik Borges <v1k@protonmail.com>
2025-02-26 22:38:20 +0700

    🚀

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SolidLitequeen::Engine.routes.draw do
   resources :databases, only: [ :index, :show ] do
-    get ":table", to: "databases#table_rows", as: :table_rows
+    get "/tables/:table", to: "databases#table_rows", as: :table_rows
   end
   root to: "databases#index"
 end
