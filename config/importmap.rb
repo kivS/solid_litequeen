0a8285c1494b2e33c63405c936f35f67523ec671
Vik Borges <v1k@protonmail.com>
2025-03-03 20:35:28 +0700

    🚀

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "application", to: "solid_litequeen/application.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from SolidLitequeen::Engine.root.join("app/javascript/solid_litequeen/jobs/controllers"), under: "controllers", to: "solid_litequeen/jobs/controllers"
-pin_all_from SolidLitequeen::Engine.root.join("app/javascript/solid_litequeen/jobs/helpers"), under: "helpers", to: "solid_litequeen/jobs/helpers"
+pin_all_from SolidLitequeen::Engine.root.join("app/javascript/solid_litequeen/controllers"), under: "controllers", to: "solid_litequeen/controllers"
+pin_all_from SolidLitequeen::Engine.root.join("app/javascript/solid_litequeen/helpers"), under: "helpers", to: "solid_litequeen/helpers"
