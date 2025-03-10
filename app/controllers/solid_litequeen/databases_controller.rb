7eabdb410dd69a90fe0c9866f061416ee67c3dfc
Vik Borges <v1k@protonmail.com>
2025-03-10 12:48:24 +0700

    🚀

--- app/controllers/solid_litequeen/databases_controller.rb
+++ app/controllers/solid_litequeen/databases_controller.rb
@@ -5,9 +5,6 @@ module SolidLitequeen
 
   class DatabasesController < ApplicationController
     def index
-      @databases = ActiveRecord::Base.configurations.configurations.select do |config|
-        config.adapter == "sqlite3" && config.env_name == Rails.env && config.database.present?
-      end
     end
 
     def show
