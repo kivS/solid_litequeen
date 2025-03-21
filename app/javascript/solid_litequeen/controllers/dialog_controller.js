ba489740058946ef7f9c910c9f95718e7d976812
Vik Borges <v1k@protonmail.com>
2025-03-21 17:55:08 +0700

    Add dialog controller for handling dialog interactions and update dialog elements in views

---
+++ app/javascript/solid_litequeen/controllers/dialog_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="dialog"
+export default class extends Controller {
+	connect() {
+		// this.element === dialog
+		this.element.addEventListener(
+			"click",
+			this.handleClickOusideDialog.bind(this),
+		);
+	}
+
+	disconnect() {
+		this.element.removeEventListener(
+			"click",
+			this.handleClickOusideDialog.bind(this),
+		);
+
+		this.element?.close();
+	}
+
+	/**
+	 * Handles mouse events for the search dialog.
+	 * @param {MouseEvent} event - The mouse event triggered by user interaction.
+	 */
+	handleClickOusideDialog(event) {
+		const rect = this.element.getBoundingClientRect();
+		const isInDialog =
+			rect.top <= event.clientY &&
+			event.clientY <= rect.top + rect.height &&
+			rect.left <= event.clientX &&
+			event.clientX <= rect.left + rect.width;
+		if (!isInDialog) {
+			this.element?.close();
+		}
+	}
+}
