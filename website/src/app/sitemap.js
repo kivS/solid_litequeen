d0258690405e579b75881fa63dfbccb8756cb246
Vik Borges <v1k@protonmail.com>
2025-03-25 18:19:45 +0700

    🚀

--- website/src/app/sitemap.js
+++ website/src/app/sitemap.js
@@ -2,18 +2,17 @@ import { promises as fs } from "fs";
 import path from "path";
 
 async function loadData() {
-	// const jsonDirectory = path.join(process.cwd(), "src/app/guides/posts.json"); // Adjust the path as necessary
-	// const fileContents = await fs.readFile(jsonDirectory, "utf8");
-	// const posts = JSON.parse(fileContents); // Parse the JSON data
+	const jsonDirectory = path.join(process.cwd(), "src/app/guides/posts.json"); // Adjust the path as necessary
+	const fileContents = await fs.readFile(jsonDirectory, "utf8");
+	const posts = JSON.parse(fileContents); // Parse the JSON data
 	// console.log({ posts });
-	// return posts;
-	return [];
+	return posts;
 }
 
 export default async function sitemap() {
 	const posts = await loadData();
 
-	console.log({ posts });
+	// console.log({ posts });
 
 	return [
 		{
