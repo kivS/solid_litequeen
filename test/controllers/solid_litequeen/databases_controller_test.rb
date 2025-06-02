aa456f27c317ef0bcfdd98be34cbbddf50392dff
Vik <v1k@protonmail.com>
2025-06-02 12:14:30 +0700

    Add enum support for table rows and tests

--- test/controllers/solid_litequeen/databases_controller_test.rb
+++ test/controllers/solid_litequeen/databases_controller_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
+require "base64"
+require "nokogiri"
 
 module SolidLitequeen
   class DatabasesControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
-    # test "the truth" do
-    #   assert true
-    # end
+    test "enum mappings include article status" do
+      Article.first
+      mappings = ::SolidLitequeen::DatabasesController.new.send(:enum_mappings)
+      assert_equal({"status" => {"draft" => 0, "published" => 1}}, mappings["articles"])
+    end
   end
 end
--- test/dummy/app/models/article.rb
+++ test/dummy/app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  enum :status, { draft: 0, published: 1 }
 end
